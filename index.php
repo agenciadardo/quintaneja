166a6076e4b5bade7c6fd3eed4daef89a49b816d
Marcos Garcia de Lima <marckfree@gmail.com>
2012-08-13 12:12:54 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <meta name="author" content="Marcker: marckfree at gmail.com" />
+        <link rel="stylesheet" href="css/styles.css" />
+        <script src="js/source.js"></script> 
+        <title>Quintaneja &#8212; A maior quinta sertaneja do ABC</title>
+    </head>
+    <body>
+        <div id="container">
+            
+        </div>
+    </body>
+</html>
