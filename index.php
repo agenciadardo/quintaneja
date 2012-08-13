5c21ca6872248c859f5a51091073b0f0295237ab
Marcos Garcia de Lima <marckfree@gmail.com>
2012-08-13 12:39:10 -0300

    Implementação do markup do cabeçalho e 960gs

--- index.php
+++ index.php
@@ -5,13 +5,45 @@
         <meta name="keywords" content="" />
         <meta name="description" content="" />
         <meta name="author" content="Marcker: marckfree at gmail.com" />
+        <link rel="stylesheet" href="css/reset.css" />
+        <link rel="stylesheet" href="css/text.css" />
+        <link rel="stylesheet" href="css/960.css" />
         <link rel="stylesheet" href="css/styles.css" />
-        <script src="js/source.js"></script> 
+        <script src="js/scripts.js"></script> 
         <title>Quintaneja &#8212; A maior quinta sertaneja do ABC</title>
     </head>
     <body>
-        <div id="container">
+        <div id="container" class="container_12">
             
-        </div>
+            <div id="cabecalho">
+                
+                <div class="logotipo grid_12">
+                    <a href="#" title="Quintaneja &#8212; A maior quinta sertaneja do ABC">
+                        <img src="" alt="Quintaneja &#8212; A maior quinta sertaneja do ABC" />
+                    </a>
+                </div>
+                
+                <div class="slogan grid_12">
+                    <h1>A melhor festa do ABC</h1>
+                </div>
+
+                <div class="navegacao grid_12">
+                    <div class="menu">
+                        <ul>
+                            <li><a href="#" title="Inicial">Inicial</a></li>
+                            <li><a href="#" title="A quintaneja">A quintaneja</a></li>
+                            <li><a href="#" title="Agenda">Agenda</a></li>
+                            <li><a href="#" title="Aniversário">Aniversário</a></li>
+                            <li><a href="#" title="Ingressos">Ingressos</a></li>
+                            <li><a href="#" title="Fotos &amp; Vídeos">Fotos &amp; Vídeos</a></li>
+                            <li><a href="#" title="Contato">Contato</a></li>
+                        </ul>
+                    </div> <!-- endOf Menu -->
+                </div> <!-- endOf Navegação -->
+
+            </div> <!-- endOf Cabeçalho -->
+
+
+        </div> <!-- endOf Container -->
     </body>
 </html>
