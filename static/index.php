0577a8e611907df6dbed1e0959f476a34c6c8eef
Marcos Garcia de Lima <marckfree@gmail.com>
2012-09-25 14:06:48 -0300

    Trocando de workstation

---
+++ static/index.php
@@ -0,0 +1,178 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="" />
+        <meta name="description" content="" />
+        <meta name="author" content="Marcker: marckfree at gmail.com" />
+        <link rel="stylesheet" href="css/reset.css" />
+        <link rel="stylesheet" href="css/text.css" />
+        <link rel="stylesheet" href="css/960.css" />
+        <link rel="stylesheet" href="css/styles.css" />
+        <script src="js/scripts.js"></script> 
+        <title>Quintaneja &#8212; A maior quinta sertaneja do ABC</title>
+    </head>
+    <body>
+ 
+        <div id="cabecalho">
+            
+            <div class="logotipo container_12">
+                <a href="#" title="Quintaneja &#8212; A maior quinta sertaneja do ABC">
+                    <img src="imagens/logotipo-quintaneja.png" alt="Quintaneja &#8212; A maior quinta sertaneja do ABC" />
+                </a>
+            </div>
+            
+            <div class="slogan container_12">
+                <h1>A melhor festa do ABC</h1>
+            </div>
+
+            <div class="navegacao">
+                <div class="menu container_12">
+                    <ul>
+                        <li><a href="#" title="Inicial">Inicial</a></li>
+                        <li><a href="#" title="A quintaneja">A quintaneja</a></li>
+                        <li><a href="#" title="Agenda">Agenda</a></li>
+                        <li><a href="#" title="Aniversário">Aniversário</a></li>
+                        <li><a href="#" title="Ingressos">Ingressos</a></li>
+                        <li><a href="#" title="Fotos &amp; Vídeos">Fotos &amp; Vídeos</a></li>
+                        <li><a href="#" title="Contato">Contato</a></li>
+                    </ul>
+                </div> <!-- endOf Menu -->
+            </div> <!-- endOf Navegação -->
+
+        </div> <!-- endOf Cabeçalho -->
+
+        <div id="container" class="container_12">
+            
+            <div id="banner-rotativo" class="alpha grid_12 omega">
+
+                <div class="banner">
+                    <img src="imagens/banner.jpg" alt="" />
+                </div> <!-- endOf Banner -->
+
+            </div> <!-- endOf Banner Rotativo -->
+
+            <div id="conteudo" class="alpha grid_8 omega">
+                <div class="boas-vindas">
+                    <div class="cabecalho-box">
+                        <h1>Bem-vindo a nossa festa!</h1>
+                    </div>
+
+                    <div class="mensagem-boas-vindas">
+                        <p>
+                            A nossa festa sertaneja acontece todas as quintas-feiras
+                            do mês, trazendo sempre uma ótima atração.
+                            São diversos artistas da região que agitam a nossa
+                            noite, sempre com muita música, alegria e 
+                            descontração.
+                            <br />
+                            Gente bonita, mulheres VIP até as 00h, segurança,
+                            premiações e muito mais! Venha curtir com a gente
+                            e traga seus amigos para nossa festa!
+                            <br />
+                            Isso tudo faz da Quintaneja a melhor quinta-feira
+                            do ABC!!!
+                        </p>
+                    </div> <!-- endOf Mensagem de boas-vindas -->
+                </div> <!-- endOf Boas-vindas -->
+
+                <div class="fotos-artistas">
+                    <div class="artistas">
+                        <img src="imagens/artistas.png" alt="" />
+                    </div>
+                </div>
+
+
+                <div class="galeria-fotos">
+                    <div class="cabecalho-box">
+                        <h1>Galeria de fotos</h1>
+                    </div>
+                    
+                    <div class="album-fotos">
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                    </div> <!-- endOf Álbum de fotos -->
+                </div><!--  endOf Galeria de fotos -->
+
+                <div class="facebook-app">
+                    <p>Facebook</p>                
+                </div> <!-- endOf Facebook App -->
+
+            </div> <!-- endOf Conteúdo -->
+
+            <div id="barra-lateral-direita" class="grid_4">
+
+                <div class="videos">
+                    <div class="cabecalho-box">
+                        <h1>Vídeos</h1>
+                    </div>
+
+                    <div class="video-embedded">
+                        <img src="" alt="" />
+                    </div> <!-- endOf Vídeo embedded -->
+                </div> <!-- endOf Vídeos -->
+
+                <div class="player-musica">
+                    <div class="cabecalho-box">
+                        <h1>Música</h1>
+                    </div>
+
+                    <div class="player">
+
+                    </div> <!-- endOf Player -->
+                </div> <!-- endOf Player de música -->
+
+                <div class="ingressos">
+
+                    <div class="cabecalho-box">
+                        <h1>Ingressos</h1>
+                    </div>
+
+                    <div class="ingresso-antecipado">
+                        <div class="mensagem-ingresso">
+                            <h2>Compre os ingressos antecipados</h2>
+                        </div> <!-- endOf Mensagem sobre ingressos antecipados -->
+                    </div> <!-- endOf Ingresso Antecipado -->
+
+                    <div class="pontos-de-venda">
+                        <div class="mensagem-pontos-de-venda">
+                            <h2>Conheça os pontos de venda</h2>
+                        </div> <!-- endOf Mensagem sobre pontos de venda -->
+                    </div> <!-- endOf Pontos de venda -->
+
+                </div> <!-- endOf Ingressos -->
+
+            </div> <!-- endOf Barra lateral direita -->
+
+        </div> <!-- endOf Container -->
+
+        <!--[if lt IE 9]>
+            <div id="rodape-ie">
+        <![endif]-->
+
+        <!--[if !IE]><!-->
+            <div id="rodape">        
+        <!--<![endif]-->
+            <p>
+                Todos os direitos reservados a Festa Quintaneja &#8212; 2012
+            </p>
+
+            <p>
+                <a href="http://www.agenciadardo.com.br">
+                    Dardo &amp; Dagny
+                </a>
+            </p>
+        </div> <!-- endOf Rodapé -->
+
+    </body>
+</html>
